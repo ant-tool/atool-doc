5a295186d28d5bbc98ddff8d96cd581bb8c321d4
yuntao.qyt <yuntao.qyt@antfin.com>
2017-04-17 17:00:00 +0800

    feat:keep webpack alias and plugins instead of create new

--- src/getWebpackConfig.js
+++ src/getWebpackConfig.js
@@ -51,10 +51,10 @@ export default function (source, asset, dest, cwd, tpl, config) {
   webpackConfig.demoSource = source;
 
   webpackConfig.resolve.root = cwd;
-  webpackConfig.resolve.alias = {
+  webpackConfig.resolve.alias = Object.assign({}, webpackConfig.resolve.alias, {
     [`${pkg.name}$`]: join(cwd, 'index.js'),
     [pkg.name]: cwd,
-  };
+  });
 
   webpackConfig.resolve.modulesDirectories.push(join(root, 'node_modules'));
   webpackConfig.resolveLoader.modulesDirectories.push(join(root, 'node_modules'));
@@ -110,7 +110,7 @@ export default function (source, asset, dest, cwd, tpl, config) {
     link[path.relative(source, key)] = key;
   });
 
-  webpackConfig.plugins = [
+  webpackConfig.plugins.push(
     new ProgressPlugin((percentage, msg) => {
       const stream = process.stderr;
       if (stream.isTTY && percentage < 0.71) {
@@ -130,7 +130,7 @@ export default function (source, asset, dest, cwd, tpl, config) {
       },
       title: 'title',
     }),
-  ];
+  );
   webpackConfig.externals = {};
   return webpackConfig;
 }
