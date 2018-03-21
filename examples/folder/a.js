892f9053737af90fa93e422a5bd7f8d7fca98c7c
绯一 <jaredlee.ljj@antfin.com>
2018-03-21 21:00:08 +0800

    Updates

--- examples/folder/a.js
+++ examples/folder/a.js
@@ -1,17 +1,17 @@
-webpackJsonp([4,0],{
-
-/***/ 0:
+webpackJsonp([4,0],[
+/* 0 */
 /***/ (function(module, exports, __webpack_require__) {
 
 	'use strict';
 
-	var _insert = __webpack_require__(13);
+	var _insert = __webpack_require__(3);
 
 		(0, _insert.insert)(document.getElementById('__element'));
 
 /***/ }),
-
-/***/ 13:
+/* 1 */,
+/* 2 */,
+/* 3 */
 /***/ (function(module, exports) {
 
 	'use strict';
@@ -25,6 +25,5 @@ webpackJsonp([4,0],{
 	}
 
 /***/ })
-
-});
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZXhhbXBsZXMvZm9sZGVyL2EuanMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vZXhhbXBsZXMvZm9sZGVyL2EubWQiLCJ3ZWJwYWNrOi8vL2V4YW1wbGVzL2ZvbGRlci9pbnNlcnQuanMiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsgaW5zZXJ0IH0gZnJvbSAnLi9pbnNlcnQnO1xuaW5zZXJ0KGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKCdfX2VsZW1lbnQnKSk7XG5cblxuLy8gV0VCUEFDSyBGT09URVIgLy9cbi8vIGV4YW1wbGVzL2ZvbGRlci9hLm1kIiwiZXhwb3J0IGZ1bmN0aW9uIGluc2VydChlbGVtZW50KSB7XG4gIGVsZW1lbnQuaW5uZXJIVE1MID0gJ3RoaXMgaXMgaW5zZXJ0IGJ5IGZ1bmN0aW9uIGluIGIuanMnO1xufVxuXG5cblxuLy8gV0VCUEFDSyBGT09URVIgLy9cbi8vIGV4YW1wbGVzL2ZvbGRlci9pbnNlcnQuanMiXSwibWFwcGluZ3MiOiI7Ozs7Ozs7QUFBQTtBQUNBO0FBQUE7Ozs7Ozs7Ozs7OztBQ0RBO0FBQUE7QUFDQTs7Ozs7Iiwic291cmNlUm9vdCI6IiJ9
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZXhhbXBsZXMvZm9sZGVyL2EuanMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vZXhhbXBsZXMvZm9sZGVyL2EubWQiLCJ3ZWJwYWNrOi8vL2V4YW1wbGVzL2ZvbGRlci9pbnNlcnQuanMiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsgaW5zZXJ0IH0gZnJvbSAnLi9pbnNlcnQnO1xuaW5zZXJ0KGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKCdfX2VsZW1lbnQnKSk7XG5cblxuLy8gV0VCUEFDSyBGT09URVIgLy9cbi8vIGV4YW1wbGVzL2ZvbGRlci9hLm1kIiwiZXhwb3J0IGZ1bmN0aW9uIGluc2VydChlbGVtZW50KSB7XG4gIGVsZW1lbnQuaW5uZXJIVE1MID0gJ3RoaXMgaXMgaW5zZXJ0IGJ5IGZ1bmN0aW9uIGluIGIuanMnO1xufVxuXG5cblxuLy8gV0VCUEFDSyBGT09URVIgLy9cbi8vIGV4YW1wbGVzL2ZvbGRlci9pbnNlcnQuanMiXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBO0FBQ0E7QUFBQTs7Ozs7Ozs7Ozs7OztBQ0RBO0FBQUE7QUFDQTs7OzsiLCJzb3VyY2VSb290IjoiIn0=
